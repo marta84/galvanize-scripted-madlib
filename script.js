d32d6b1fa47ec0afe9e6a4d7e8aecc55c2d6eaaf
Parker Lewis <parker.lewis14@att.net>
2017-07-13 16:24:38 -0700

    update script.js

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ $(document).ready(function() {
         var shirtColor = "yellow";
         var shoesColor = "green";
         var linesOfCode = 123;
-        var linesOfCodeModifier = modifierFunc(linesOfCode);
-        var lessonLearned = "jQuery"
+        var linesOfCodeModifier = "foobar";
+        var lessonLearned = "jQuery";
         
-        function modifierFunc(lines) {
-            /* Your code here */
-        }
+        /* 
+            Write your function named "modifierFunc" here.
+
+            The argument for this function should be named
+            "lines". 
+
+            Inside this function there should be an 
+            if/else statement that returns different values
+            depending on the value of the "lines" parameter.
+            (See the directions in README.md for more info)
+
+            You will need call the function by passing
+            in the variable "linesOfCode" and then save the return 
+            value in the variable named "linesOfCodeModifier"
+            above. 
+        */
     
         var returnString =      "I had a great time at Galvanize with Parker today but his look needs"
                             +   " some serious help! First off, he should dye his hair " + hairColor + "."
                             +   " He also really needs a " + shirtColor + " shirt to go with some " + shoesColor
                             +   " shoes. Aside from his outfit, he was a pretty good instructor. I was able " 
                             +   " to write " + linesOfCodeModifier + " more lines of code because of our review."
-                            +   " After this time at Galvanize I will never forget " + lessonLearned + "!"
+                            +   " After this time at Galvanize I will never forget " + lessonLearned + "!";
         
-        console.log(returnString)
+        console.log(returnString);
         
-    });
-    
-    
+    }); 
 });
